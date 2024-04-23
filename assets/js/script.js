3f29558144ba3ace10f3415c212f134b9151220f
d4v1-sudo <139714828+d4v1-sudo@users.noreply.github.com>
2024-04-23 09:26:20 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,24 @@ document.addEventListener("DOMContentLoaded", function() {
     var footer = document.getElementById("site-footer");
 
     if (navbar && header && router && title && footer) {
-        console.log("Successfully selected elements!");
+        console.log("Elements selected successfully!");
 
         title.addEventListener("click", function() {
-            console.log("Navbar clicked!");
+            console.log("Clicked on navbar title!");
             toggleNavbar();
         });
 
         document.addEventListener("click", function(event) {
             var target = event.target;
             if (!navbar.contains(target) && !title.contains(target)) {
-                console.log("Clicked outside the navbar!");
+                console.log("Clicked outside navbar!");
                 closeNavbar();
             }
         });
 
         document.addEventListener("keydown", function(event) {
             if (event.key === "Escape") {
-                console.log("Esc key pressed!");
+                console.log("Escape key pressed!");
                 if (navbar.classList.contains("nav-active")) {
                     closeNavbar();
                 } else {
@@ -31,12 +31,32 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             }
         });
+
+        navbar.addEventListener("mouseout", function(event) {
+            var relatedTarget = event.relatedTarget;
+            if (!navbar.contains(relatedTarget)) {
+                restoreActiveClass();
+            }
+        });
+
+        // Save the original element with the active class
+        var originalActiveLink = document.querySelector('.links a.active');
+
+        var links = document.querySelectorAll('.links a');
+        links.forEach(function(link) {
+            link.addEventListener('mouseover', function() {
+                if (!link.classList.contains('active')) {
+                    removeActiveClassFromLinks();
+                    link.classList.add('active');
+                }
+            });
+        });
     } else {
         console.log("Error: One or more elements were not found!");
     }
 
     function toggleNavbar() {
-        console.log("Switching navbar status...");
+        console.log("Toggling navbar state...");
         if (navbar.classList.contains("nav-active")) {
             closeNavbar();
         } else {
@@ -45,7 +65,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function openNavbar() {
-        console.log("Abrindo navbar...");
+        console.log("Opening navbar...");
         header.classList.add("nav-active");
         navbar.classList.add("nav-active");
         router.classList.add("nav-active", "dimmable-active");
@@ -53,22 +73,24 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function closeNavbar() {
-        console.log("Fechando navbar...");
+        console.log("Closing navbar...");
         header.classList.remove("nav-active");
         navbar.classList.remove("nav-active");
         router.classList.remove("nav-active", "dimmable-active");
         footer.classList.remove("nav-active", "dimmable-active");
     }
-});
 
-function toggleLanguageSelect() {
-    var languageSelect = document.querySelector('.language-select');
-    var changeLang = document.querySelector('.change-lang');
-    var changeLang2 = document.querySelector('.change-lang2');
-
-    if (languageSelect && changeLang && changeLang2) {
-        languageSelect.classList.toggle('active');
-        changeLang.classList.toggle('active');
-        changeLang2.classList.toggle('active');
+    function removeActiveClassFromLinks() {
+        var links = document.querySelectorAll('.links a');
+        links.forEach(function(link) {
+            link.classList.remove('active');
+        });
     }
-}
+
+    function restoreActiveClass() {
+        removeActiveClassFromLinks();
+        if (originalActiveLink) {
+            originalActiveLink.classList.add('active');
+        }
+    }
+});
